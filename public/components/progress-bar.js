a65920ee9402aedab046a38c614f907d31391744
herods <rodi.centr@gmail.com>
2024-09-04 14:49:49 +0300

    refactor code

--- public/components/progress-bar.js
+++ public/components/progress-bar.js
@@ -2,12 +2,12 @@ class ProgressBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <!-- Coin Progress Bar -->
-            <div id="coin-progress-bar” class="progress mb-3">
+            <div id="coin-progress-bar" class="progress mb-3">
                 <!-- Initially, the progress bar is empty -->
-                <div id="coin-progress” class="progress-bar” role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="5"></div>
-                <img src="Images/CoinIcon.png" alt="Coin Icon” class="img-fluid ml-3" style="width: 40px; height: 40px;">
+                <div id="coin-progress" class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="5"></div>
+                <img src="images/coin-icon.png" alt="Coin Icon" class="img-fluid" style="width: 40px; height: 40px;">
             </div>
-            <p id="task-message”></p>
+            <p id="task-message"></p>
         `
     }
 }
--- public/scripts/progress-bar.js
+++ public/scripts/progress-bar.js
@@ -1,29 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const coinProgress = document.getElementById("coin-progress");
-  const taskMessage = document.getElementById("task-message");
 
   function updateProgressBar() {
     const allTasks = document.getElementsByClassName('task-card');
-    const tasksInDoneColumn = document.querySelector('#DONE-column task-card');
-
+    const tasksInDoneColumn = document.querySelectorAll('#DONE-column .task-card');
     const numberOfAlltasks = allTasks.length;
     const numberOfDoneTasks = tasksInDoneColumn ? tasksInDoneColumn.length : 0;
 
-    const move = 20;
-
-    const progressPercentage = (numberOfDoneTasks % numberOfAlltasks) * move;
+    const progressPercentage = (numberOfDoneTasks / numberOfAlltasks) * 100;
     coinProgress.style.width = `${progressPercentage}%`;
     coinProgress.setAttribute("aria-valuenow", numberOfDoneTasks % numberOfAlltasks);
-    if (numberOfDoneTasks % numberOfDoneTasks === 0) {
-      taskMessage.textContent = `Congratulations! You’ve completed ${numberOfDoneTasks} tasks`;
-      setTimeout(() => {
-        taskMessage.textContent = "";
-        coinProgress.style.width = "0";
-      }, 3000);
-    }
   }
 
   updateProgressBar();
+  window.updateProgressBar = updateProgressBar;
 
-  document.updateProgressBar = updateProgressBar;
 });
